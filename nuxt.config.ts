14d0ff1a94e1434c5378843acf45adb88eee971a
OTTIN-T <ottin.timoth@gmail.com>
2024-01-04 14:15:30 +0100

    [add] vercel analytics + speed insights. [update] change security header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,7 +66,7 @@ export default defineNuxtConfig({
   },
   security: {
     headers: {
-      crossOriginEmbedderPolicy: process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'require-corp',
+      crossOriginEmbedderPolicy: process.env.NODE_ENV === 'development' ? 'unsafe-none' : 'credentialless',
     },
   },
   pwa,
